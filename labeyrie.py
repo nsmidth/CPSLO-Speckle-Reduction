29f5d8c3790e630d32ddf63035e2cf267fc583c2
Niels <nsmidth@gmail.com>
2016-01-08 16:30:37 -0800

    Creating new files to be main program files

---
+++ labeyrie.py
@@ -0,0 +1,25 @@
+# Module for performing full Labeyrie deconvolution of FITS files
+
+
+# If preprocessing:
+# Import binary star FITS
+# Preprocess it
+# Save it to binary star object
+# Import reference star FITS
+# Preprocess it
+# Save it to reference star object
+
+# If not preprocessing
+# Import binary star PSD FITS
+# Save it to a binary star object
+# Import reference star PSD FITS
+# Save it to a reference star object
+
+# Deconvolve reference and binary stars
+# Save result to output star object
+
+# Perform filtering on output star object
+
+# iFFT to get autocorrelogram
+
+# Return autocorrelogram
