77dd6ced9953f9c414a06ce26a88f98ded98fa02
Niels <nsmidth@gmail.com>
2016-01-11 11:25:29 -0800

    Commented in all basic commands

--- labeyrie.py
+++ labeyrie.py
@@ -1,24 +1,50 @@
 # Module for performing full Labeyrie deconvolution of FITS files
 
-# If preprocessing:
-# Import binary star FITS
-# Preprocess it
-# Save it to binary star object
-# Import reference star FITS
-# Preprocess it
-# Save it to reference star object
+# Included modules
+import sys
+from labeyrieClasses import target,deconvolved
 
-# If not preprocessing
-# Import binary star PSD FITS
-# Save it to a binary star object
-# Import reference star PSD FITS
-# Save it to a reference star object
+# Instantiate objects
+binary = target()
+reference = target()
+dcnvlv = deconvolved()
 
+# Prompt user if they'd like to preprocess:
+if ( input("Preprocessing raw FITS files? [y/n]   ").lower() == 'y' ):
+	# Prompt for binary FITS file location
+	#binary.fitsFileName = 
+	# Import binary FITS
+	#binary.fitsImport()
+	# Preprocess
+	#binary.PSDCalc()
+
+	# Prompt for reference FITS file location
+	#reference.fitsFileName = 
+	# Import reference FITS
+	#reference.fitsImport()
+	# Preprocess
+	#reference.PSDCalc()
+	
+	pass
+else:
+	# Prompt user for binary PSD file location
+	#binary.psdFileName = 
+	# Import binary star PSD FITS
+	#binary.psdImport()
+	
+	# Prompt user for reference PSD file location
+	#reference.psdFileName = 
+	# Import reference star PSD FITS
+	#reference.psdImport()
+	pass
+	
 # Deconvolve reference and binary stars
-# Save result to output star object
+#dcnvlv.psdDeconvolve(binary.PSD,reference.PSD)
 
 # Perform filtering on output star object
+#dcnvlv.psdFilter(lpf = 15, interference = TRUE)
 
-# iFFT to get autocorrelogram
+# Get autocorrelogram
+#dcnvlv.acorrCalc()
 
-# Return autocorrelogram as PNG or FIT
+# Return autocorrelogram as PNG or FITS
