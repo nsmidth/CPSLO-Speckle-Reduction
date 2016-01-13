f589e0a37b98167a0a742eb82e8507bd49f85514
Niels <nsmidth@gmail.com>
2016-01-13 08:24:01 -0800

    Made spaces/tabs consistent

--- labeyrieClasses.py
+++ labeyrieClasses.py
@@ -14,7 +14,7 @@ class target():
     psd = None				# Holds PSD of target
     psdFileName	= None		# Holds filename of PSD file
 	
-	# Import FITS data from filename
+    # Import FITS data from filename
     #  filePath = file path of FITS file
     #  printInfo = print FITS file information?
     #  printHeaders = print FITS file headers?
@@ -22,31 +22,31 @@ class target():
         # Use fitsFileName as filePath by default
         if filePath == None:
             filePath = self.fitsFileName				
-		# Open FITS Data
+        # Open FITS Data
         HDUList = fits.open(filePath)  
-		# Print FITS File Info & Headers
+        # Print FITS File Info & Headers
         if (printInfo == True):
-            HDUList.info()
+        HDUList.info()
             print()
 
         if (printHeaders == True):
             print("Headers:")
             print(repr(HDUList[0].header))
-		# Save data in FITS cube to local variable, then close FITS file
+        # Save data in FITS cube to local variable, then close FITS file
         fitsData = HDUList[0].data
         HDUList.close()
-		# Return FITS data
+        # Return FITS data
         return fitsData
 
-	# View an image from original FITS
+    # View an image from original FITS
     #  imNum = index of image to be printed/returned
     def fitsView(self, imNum=0):
-        plt.figure()        
+        plt.figure()
         plt.imshow(self.fits[imNum])
         plt.title('FITS Image ' + str(imNum))
         plt.show()
 
-	# fitsExport(): Export FITS file of a np array
+    # fitsExport(): Export FITS file of a np array
 
 	# psdImport(): Import PSD data (from a FITS file)
 	# psdCalc(): Calculate PSD of target
