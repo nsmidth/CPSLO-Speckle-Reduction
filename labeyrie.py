e2b65016542040383e50554e59a7b2aa31dc2e6b
Niels <nsmidth@gmail.com>
2016-01-14 08:56:19 -0800

    Importing PSD files

--- labeyrie.py
+++ labeyrie.py
@@ -6,9 +6,10 @@ import sys
 from labeyrieClasses import target,deconvolved
 import tkinter as tk
 from tkinter import filedialog
+import os, sys
 
 # Start up Tkinter
-root = tk.Tk()  
+root = tk.Tk()
 root.withdraw()
 
 # Instantiate objects
@@ -17,14 +18,16 @@ reference = target()
 dcnvlv = deconvolved()
 
 # Prompt user for binary PSD file location
-#binary.psdFileName = 
+binary.psdFileName = filedialog.askopenfilename(title="Select BINARY FITS file")
 # Import binary star PSD FITS
-#binary.psdImport()
+binary.psdImport()
+#binary.psdView()
 
 # Prompt user for reference PSD file location
-#reference.psdFileName = 
+reference.psdFileName = filedialog.askopenfilename(title="Select REFERENCE FITS file")
 # Import reference star PSD FITS
-#reference.psdImport()
+reference.psdImport()
+#reference.psdView()
 
 # Deconvolve reference and binary stars
 #dcnvlv.psdDeconvolve(binary.PSD,reference.PSD)
