1a60d1ce1ddeeb6afdeacfdbe4e45c121d3c96d0
Niels <nsmidth@gmail.com>
2016-01-19 11:00:24 -0800

    Testing markup of Acorr

--- labeyrie.py
+++ labeyrie.py
@@ -3,8 +3,9 @@
 
 # Included modules
 import sys
-from labeyrieClasses import target,deconvolved
+from labeyrieClasses import target,deconvolved, photometry
 import tkinter as tk
+import matplotlib.pyplot as plt
 from tkinter import filedialog
 import os, sys
 
@@ -15,29 +16,43 @@ root.withdraw()
 # Instantiate objects
 binary = target()
 reference = target()
-soln = deconvolved()
+deconv = deconvolved()
+calcs = photometry()
 
 # Prompt user for binary PSD file location
-binary.psd.fileName = filedialog.askopenfilename(title="Select BINARY FITS file")
+#binary.psd.fileName = filedialog.askopenfilename(title="Select BINARY FITS file")
+binary.psd.fileName = "/home/niels/Desktop/KP330_PSD.fits"
 # Import binary star PSD FITS
 binary.psd.read()
-binary.psd.view(log=True)
 
 # Prompt user for reference PSD file location
-reference.psd.fileName = filedialog.askopenfilename(title="Select REFERENCE FITS file")
+#reference.psd.fileName = filedialog.askopenfilename(title="Select REFERENCE FITS file")
+reference.psd.fileName = "/home/niels/Desktop/KP331_PSD.fits"
 # Import reference star PSD FITS
 reference.psd.read()
-reference.psd.view(log=True)
 
 # Deconvolve reference and binary stars
-soln.psdDeconvolve(binary.psd.data,reference.psd.data,1e-12)
+deconv.psdDeconvolve(binary.psd.data,reference.psd.data,1e-12)
 
 # Perform filtering on output star object
-soln.psdFilter(lpfRadius = 25, interference=True)
+deconv.psdFilter(lpfRadius = 25, interference=True)
 
 # Get autocorrelogram
-soln.acorrCalc()
+deconv.acorrCalc()
 
 # View Results
-soln.psdFiltered.view(log=True)
-soln.acorr.view()
+deconv.acorr.view()
+
+# Estimating centroid locations
+# Moving acorr to photometry object
+calcs.acorr = deconv.acorr.data
+calcs.acorrMarkedClear()
+
+#Testing marking of Acorr function
+calcs.acorrMark(x=10,y=200,shape='o',color=(255,0,0))
+calcs.acorrMark(x=200,y=200,shape="+",color=(0,255,0))
+
+# View Marked Acorr
+plt.figure()
+plt.imshow(calcs.acorrMarked)
+plt.show()
